d045d280abac74d0b02374a88c9dd0e34b36f5fd
mart-Velema <mart.velema@student.nhlstenden.com>
2024-03-27 11:27:21 +0100

    Cleaned up IR sensor code

--- battleBot/IrSensors.ino
+++ battleBot/IrSensors.ino
@@ -1,3 +1,4 @@
+//TODO Remove when race day
 void readIrOutput()
 {
   for (int sensorPin : IR_SENSORS)
@@ -9,87 +10,12 @@ void readIrOutput()
   Serial.println();
 }
 
-void getIrColors()
+boolean isOnLightColor()
 {
-    int i = 0;
-    for (int sensorPin : IR_SENSORS)
-    {
-      int value = analogRead(sensorPin);
-      if (value > sensorTresholds[i][1])
-      {
-        sensorColor[i] = false;
-
-      }
-      else if (value < sensorTresholds[i][0])
-      {
-        sensorColor[i] = true;
-      }
-
-      if(sensorColor[i])
-      {
-        Serial.print("White");  
-      }
-      else
-      {
-        Serial.print("Black");          
-      }
-      i++;
-      Serial.print(" ");
-    }
-    Serial.println();
-}
-
-void setIrThresholds(int time)
-{
-  // Lijst structuur voor thresholds opslaan [[200, 300], [100, 293]] : [[whiteThreshold, blackThreshold], ...]
-  
-  // Zet witte tresholds
-  int runtime = millis() + time;
-  Serial.println("Detecting white");
-  while (runtime > millis())
+  int averageColor = 0;
+  for (int sensorPin : IR_SENSORS)
   {
-    updateThreshold(0);
+    averageColor += analogRead(sensorPin);
   }
-
-  // Wacht op hogere waarden
-  while (true)
-  {
-    int i = 0;
-    bool detectedWhite = false;
-    for (int sensorPin : IR_SENSORS)
-    {
-      if (sensorTresholds[i][0] + 200 > analogRead(sensorPin))
-      {
-        detectedWhite = true;
-        break;
-      }  
-      i++;
-    }
-    if(!detectedWhite)
-    {
-      break;  
-    }
-  }
- 
-  // Zet zwarte tresholds
-  runtime = millis() + time;
-  Serial.println("Detecting black");
-  while (runtime > millis())
-  {
-    updateThreshold(1);
-  }
-}
-
-void updateThreshold(int index)
-{
-    int i = 0;
-    for (int sensorPin : IR_SENSORS)
-    {
-      int value = analogRead(sensorPin);
-      if(value > sensorTresholds[i][index])
-      {
-        sensorTresholds[i][index] = value;
-      }
-      i++;
-    }
+  return (averageColor / 4) <= 800;
 }
